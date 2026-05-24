a350b10d23beef9bfd02538598e8f4c279c0b0e1
Nabhan <jawahiirboss@gmail.com>
2026-05-25 00:00:33 +0600

    update create-user dto to use class variance & class-validator.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class CreateUserDto {
-  constructor(
-    public readonly email: string,
-    public readonly password?: string | null,
-    public readonly fullName?: string,
-    public readonly googleId?: string | null,
-    public readonly isPending?: boolean,
-    public readonly isActive?: boolean,
-  ) {}
+  @ApiProperty({ example: 'user@example.com' })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  password?: string | null;
+
+  @ApiProperty({ example: 'John Doe', required: false })
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  fullName?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  googleId?: string | null;
+
+  @ApiProperty({ required: false, default: false })
+  @IsOptional()
+  @IsBoolean()
+  isPending?: boolean;
+
+  @ApiProperty({ required: false, default: true })
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
 }
