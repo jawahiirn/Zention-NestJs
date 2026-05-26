9dc08634205c564a6bc8dfe7a953e7fc54a04fa3
Nabhan <jawahiirboss@gmail.com>
2026-05-26 18:32:46 +0600

    remove the workspace module imports from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,12 @@ import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { UsersModule } from './users/users.module';
 import { IamModule } from './iam/iam-infrastructure.module';
-import { WorkspacesModule } from './workspaces/application/workspaces.module';
 import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 @Module({
-  imports: [
-    CoreModule.forRoot(),
-    UsersModule,
-    IamModule,
-    WorkspacesModule,
-  ],
+  imports: [CoreModule.forRoot(), UsersModule, IamModule],
   controllers: [AppController],
   providers: [
     AppService,
